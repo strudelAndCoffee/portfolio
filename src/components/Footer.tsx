679cff4d6fe1a234544b4f350448edc46634e4a2
Bort <93967399+strudelAndCoffee@users.noreply.github.com>
2022-10-23 18:08:02 -0500

    install react-icons, add links/icons to footer

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -1,3 +1,25 @@
+import { FaGithub, FaLinkedin, FaEnvelope } from 'react-icons/fa'
+
 export default function Footer() {
-    return <footer>Footer</footer>
+    return (
+        <footer>
+            <div className="footer-left">
+                2022 Built with  by strudelAndCoffee
+                <br />
+                Photo by <a href="https://unsplash.com/@csfoto?utm_source=unsplash&utm_medium=referral&utm_content=creditCopyText">Carlos Alfonso</a> on <a href="https://unsplash.com/s/photos/austin-texas?utm_source=unsplash&utm_medium=referral&utm_content=creditCopyText">Unsplash</a>
+  
+            </div>
+            <div className="footer-right">
+                <a href="https://github.com/strudelAndCoffee" target="_blank" rel="noreferrer" className="footer-link">
+                    <FaGithub />
+                </a>
+                <a href="https://www.linkedin.com/in/stevie-trudell-atx/" target="_blank" rel="noreferrer" className="footer-link">
+                    <FaLinkedin />
+                </a>
+                <a href="mailto:strudelandcoffee@gmail.com" target="_blank" rel="noreferrer" className="footer-link">
+                    <FaEnvelope />
+                </a>
+            </div>
+        </footer>
+    )
 }
