0bae56350d7fe0cc6c5f59bf2aeee973834633f1
Bort <93967399+strudelAndCoffee@users.noreply.github.com>
2022-10-25 15:57:32 -0500

    h1 title links back to main

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -3,7 +3,9 @@ import Navbar from "./Navbar";
 export default function Header() {
     return (
         <header>
-            <h1>Stephen Trudell</h1>
+            <h1>
+                <a href='/' className='main-title'>Stephen Trudell</a>
+            </h1>
             <Navbar />
         </header>
     )
