e25e6abb733942aa25e989be412a2bd773aee293
Bort <93967399+strudelAndCoffee@users.noreply.github.com>
2022-11-10 14:46:02 -0600

    add current page to header

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,14 +1,39 @@
 import { useState } from 'react'
 import { FaAngleLeft, FaAngleRight } from 'react-icons/fa'
-import { Navbar, NavBarProps } from "./Navbar";
+import { Navbar } from "./Navbar";
 
-export function Header({ setPage }: NavBarProps) {
+type HeaderProps = {
+    page: string
+    setPage: React.Dispatch<React.SetStateAction<string>>
+}
+type PagesType = {
+    [str: string]: string
+}
+const pages: PagesType = {
+    port: 'PORTFOLIO',
+    about: 'ABOUT',
+    cert: 'CERTIFICATION',
+    resume: 'RESUME',
+    comm: 'COMMUNITY',
+    cont: 'CONTACT'
+}
+
+export function Header({ page, setPage }: HeaderProps) {
     const [ menuOpen, setMenuOpen ] = useState(false)
     return (
         <header>
             <div className='site-title' onClick={() => setPage("home")}>
                 <h1>Stephen Trudell</h1>
             </div>
+            {page !== 'home'
+                ?
+                    (<div className='current-page-container'>
+                        <FaAngleRight />
+                        <span className='current-page'>{pages[page]}</span>
+                    </div>)
+                :
+                    (<></>)
+            }
             {menuOpen
                 ?
                     (<div className='nav-menu-arrow open' onClick={() => setMenuOpen((prev) => !prev)}>
