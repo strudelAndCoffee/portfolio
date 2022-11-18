4f564d72dee8cae649c8e62021107fc25e853d40
Bort <93967399+strudelAndCoffee@users.noreply.github.com>
2022-11-18 07:55:55 -0600

    update footer

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -3,7 +3,7 @@ import { FaHeart } from 'react-icons/fa'
 export function Footer() {
     return (
         <footer>
-            2022 Built with <FaHeart /> by strudelAndCoffee
+            Built with <FaHeart /> by strudelAndCoffee <small>2022</small>
             {/* Photo by <a href="https://unsplash.com/@csfoto?utm_source=unsplash&utm_medium=referral&utm_content=creditCopyText">Carlos Alfonso</a> on <a href="https://unsplash.com/s/photos/austin-texas?utm_source=unsplash&utm_medium=referral&utm_content=creditCopyText">Unsplash</a>  */}
         </footer>
     )
