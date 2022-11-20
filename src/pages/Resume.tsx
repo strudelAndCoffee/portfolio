9fcca483c3ff6f84850862be316268c20281fd95
Bort <93967399+strudelAndCoffee@users.noreply.github.com>
2022-11-19 23:20:41 -0600

    start resume style

--- src/pages/Resume.tsx
+++ src/pages/Resume.tsx
@@ -1,48 +1,50 @@
 import doc from '../assets/lib/resume.json';
+import styles from '../css/resume.module.css';
 
 export default function Resume() {
     return (
-        <section>
-            <h2>Resume</h2>
-            <div>
-                {doc['Languages'].map(ln => (
-                    <p>{ln}</p>
-                ))}
-            </div>
-            <div>
-                {doc['Frameworks'].map(fw => (
-                    <p>{fw}</p>
-                ))}
-            </div>
-            <div>
-                {doc['Front End'].map(ss => (
-                    <p>{ss}</p>
-                ))}
-            </div>
-            <div>
-                {doc['Server Side'].map(ss => (
-                    <p>{ss}</p>
-                ))}
-            </div>
-            <div>
-                {doc['Databases'].map(ss => (
-                    <p>{ss}</p>
-                ))}
-            </div>
-            <div>
-                {doc['Deployment'].map(ss => (
-                    <p>{ss}</p>
-                ))}
-            </div>
-            <div>
-                {doc['DevOps'].map(ss => (
-                    <p>{ss}</p>
-                ))}
-            </div>
-            <div>
-                {doc['Acronym Soup'].map(ss => (
-                    <p>{ss}</p>
-                ))}
+        <section className={styles.resume}>
+            <div className={styles.container}>
+                <article className={styles.card}>
+                    {doc['Languages'].map(ln => (
+                        <p>{ln}</p>
+                    ))}
+                </article>
+                <article className={styles.card}>
+                    {doc['Frameworks'].map(fw => (
+                        <p>{fw}</p>
+                    ))}
+                </article>
+                <article className={styles.card}>
+                    {doc['Front End'].map(ss => (
+                        <p>{ss}</p>
+                    ))}
+                </article>
+                <article className={styles.card}>
+                    {doc['Server Side'].map(ss => (
+                        <p>{ss}</p>
+                    ))}
+                </article>
+                <article className={styles.card}>
+                    {doc['Databases'].map(ss => (
+                        <p>{ss}</p>
+                    ))}
+                </article>
+                <article className={styles.card}>
+                    {doc['Deployment'].map(ss => (
+                        <p>{ss}</p>
+                    ))}
+                </article>
+                <article className={styles.card}>
+                    {doc['DevOps'].map(ss => (
+                        <p>{ss}</p>
+                    ))}
+                </article>
+                <article className={styles.card}>
+                    {doc['Acronym Soup'].map(ss => (
+                        <p>{ss}</p>
+                    ))}
+                </article>
             </div>
         </section>
     )
