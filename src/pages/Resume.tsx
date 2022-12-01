f0a7e5505799cc7ca7575b6e9068409e22aeb1d6
Bort <93967399+strudelAndCoffee@users.noreply.github.com>
2022-12-01 15:49:31 -0600

    add scroll animation to resume page

--- src/pages/Resume.tsx
+++ src/pages/Resume.tsx
@@ -1,46 +1,65 @@
 import doc from '../assets/lib/resume.json';
 import styles from '../css/resume.module.css';
+import '../css/global.css';
+import { useLayoutEffect } from 'react';
+
+const observer = new IntersectionObserver(entries => {
+    entries.forEach(entry => {
+        if (entry.isIntersecting) entry.target.classList.add('show')
+        else entry.target.classList.remove('show')
+    })
+})
+
+function scrollEffect() {
+    const hiddenElements = document.querySelectorAll('.hidden')
+    hiddenElements.forEach(el => observer.observe(el))
+}
 
 export default function Resume() {
+
+    useLayoutEffect(() => {
+        scrollEffect()
+    },[])
+
     return (
         <section className={styles.resume}>
             <div className={styles.container}>
-                <article className={styles.card}>
+                <article className={`${styles.card} hidden`}>
                     {doc['Languages'].map(ln => (
                         <p>{ln}</p>
                     ))}
                 </article>
-                <article className={styles.card}>
+                <article className={`${styles.card} hidden`}>
                     {doc['Frameworks'].map(fw => (
                         <p>{fw}</p>
                     ))}
                 </article>
-                <article className={styles.card}>
+                <article className={`${styles.card} hidden`}>
                     {doc['Front End'].map(ss => (
                         <p>{ss}</p>
                     ))}
                 </article>
-                <article className={styles.card}>
+                <article className={`${styles.card} hidden`}>
                     {doc['Server Side'].map(ss => (
                         <p>{ss}</p>
                     ))}
                 </article>
-                <article className={styles.card}>
+                <article className={`${styles.card} hidden`}>
                     {doc['Databases'].map(ss => (
                         <p>{ss}</p>
                     ))}
                 </article>
-                <article className={styles.card}>
+                <article className={`${styles.card} hidden`}>
                     {doc['Deployment'].map(ss => (
                         <p>{ss}</p>
                     ))}
                 </article>
-                <article className={styles.card}>
+                <article className={`${styles.card} hidden`}>
                     {doc['DevOps'].map(ss => (
                         <p>{ss}</p>
                     ))}
                 </article>
-                <article className={styles.card}>
+                <article className={`${styles.card} hidden`}>
                     {doc['Acronym Soup'].map(ss => (
                         <p>{ss}</p>
                     ))}
