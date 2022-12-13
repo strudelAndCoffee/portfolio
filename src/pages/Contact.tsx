30f9a39c6bfe5a90d78247fb2c9d1d2e1bf313a9
Bort <93967399+strudelAndCoffee@users.noreply.github.com>
2022-12-12 19:18:07 -0600

    reset from gsap test

--- src/pages/Contact.tsx
+++ src/pages/Contact.tsx
@@ -1,3 +1,3 @@
 export default function Contact() {
-    return <section>Contact</section>
+  return <section>Contact</section>;
 }
--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,27 +1,27 @@
-import styles from '../css/home.module.css'
+import styles from '../css/home.module.css';
 
 type HomeProps = {
-    setPage: React.Dispatch<React.SetStateAction<string>>
-}
+  setPage: React.Dispatch<React.SetStateAction<string>>;
+};
 
 export default function Home({ setPage }: HomeProps) {
-    return (
-        <section className={styles.container}>
-            <div className={styles.upper}>
-                <h2>Full Stack Web Developer</h2>
-                <p>React | TypeScript | Node | MERN</p>
-            </div>
-            <div className={styles.lower}>
-                <div className={styles.card} onClick={() => setPage('port')}>
-                    Browse my portfolio
-                </div>
-                <div className={styles.card} onClick={() => setPage('resume')}>
-                    View my resume
-                </div>
-                <div className={styles.card} onClick={() => setPage('cont')}>
-                    Contact me
-                </div>
-            </div>
-        </section>
-    )
+  return (
+    <section className={styles.container}>
+      <div className={styles.upper}>
+        <h2>Full Stack Web Developer</h2>
+        <p>React | TypeScript | Node | MERN</p>
+      </div>
+      <div className={styles.lower}>
+        <div className={styles.card} onClick={() => setPage('port')}>
+          Browse my portfolio
+        </div>
+        <div className={styles.card} onClick={() => setPage('resume')}>
+          View my resume
+        </div>
+        <div className={styles.card} onClick={() => setPage('cont')}>
+          Contact me
+        </div>
+      </div>
+    </section>
+  );
 }
